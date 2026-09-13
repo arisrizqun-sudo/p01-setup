1504880c81eb1471ec28faf9b34614a58eeeaf39
arisrizqun-sudo <arisrizqun@gmail.com>
2026-09-13 20:55:58 +0700

    Perubahan informasi pertemuan pada hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import sys
-print("Praktek Pemrograman Terapan - Pertemuan 1")
+print("Praktek Pemrograman Terapan - Pertemuan 2")
 print("Nama :", "Achmad Rizqun Syazwani")
 print("NIM :", "33132501008")
 print("Python:", sys.version)
